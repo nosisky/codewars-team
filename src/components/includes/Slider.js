3a2743336de4b034508ab600c77fad75cecd2a06
Nosisky <nosisky@gmail.com>
2018-10-21 09:54:57 +0100

    Add base files

---
+++ src/components/includes/Slider.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+const Slider = () => {
+  return (
+<div class="banner-silder">
+  <div id="JiSlider" class="jislider">
+    <ul>
+      <li>
+        <div class="w3layouts-banner-top">
+          <div class="container">
+            <div class="col-md-6 bannerimg">
+            </div>
+            <div class="col-md-6 agileits-banner-info">
+              <h3>Natural & Fresh Fruits Supply</h3>
+              <h3>Quality</h3>
+               <p>Fresh Fruits from our fruit land</p>
+              <div class="more">
+                <a href="#" class="hvr-shutter-in-vertical">Read More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+              </div>
+            </div>	
+            <div class="clearfix"></div>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="w3layouts-banner-top w3layouts-banner-top1">
+          <div class="container">
+            <div class="col-md-6 bannerimg1">
+            </div>
+            <div class="col-md-6 agileits-banner-info">
+              <h3>Affordable Prices, Highest Quality</h3>
+              <h3>Products</h3>
+               <p>Fresh Fruits from our fruit land</p>
+              <div class="more">
+                <a href="#" class="hvr-shutter-in-vertical">Read More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+              </div>
+            </div>	
+            <div class="clearfix"></div>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="w3layouts-banner-top w3layouts-banner-top2">
+          <div class="container">
+            <div class="col-md-6 bannerimg2">
+            </div>
+            <div class="col-md-6 agileits-banner-info">
+              <h3>We Have Wide Range Of</h3> 
+              <h3>Products</h3>
+               <p>Fresh Fruits from our fruit land</p>
+              <div class="more">
+                <a href="#" class="hvr-shutter-in-vertical" data-toggle="modal" data-target="#myModal">Read More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+              </div>
+            </div>	
+            <div class="clearfix"></div>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="w3layouts-banner-top w3layouts-banner-top3">
+          <div class="container">
+            <div class="col-md-6 bannerimg3">
+            </div>
+            <div class="col-md-6 agileits-banner-info">
+              <h3>We Grow & Sell Organic Food</h3>
+              <h3>Collection</h3>
+               <p>Fresh Fruits from our fruit land</p>
+              <div class="more">
+                <a href="#" class="hvr-shutter-in-vertical">Read More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+              </div>
+            </div>	
+            <div class="clearfix"></div>
+          </div>
+        </div>
+      </li>
+
+    </ul>
+  </div>
+  </div>
+  );
+};
+
+export default Slider;
